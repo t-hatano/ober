db4a6fb9fb00b7c730dd98bfda073dcc99d65c3a
t-hatano <tnotawa@gmail.com>
2014-02-27 17:13:39 +0900

    リファクタリングとJavadocを適当に書いた

--- src/ob/Dictionary.java
+++ src/ob/Dictionary.java
@@ -12,6 +12,10 @@ public class Dictionary {
 
 	private HashMap<String, String> dictionaryMap = new HashMap<>();
 	
+	/**
+	 * 辞書オブジェクトを作る
+	 * @param dictionaryFileName
+	 */
 	public Dictionary(String dictionaryFileName) {
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dictionaryFileName), "UTF-8"));
@@ -36,6 +40,11 @@ public class Dictionary {
 		}
 	}
 	
+	/**
+	 * japaneseの先頭から，辞書にある単語のうち最長のものを抽出する
+	 * @param japanese
+	 * @return
+	 */
 	public String extractBeginWord(String japanese) {
 		String cand = "";
 		for (String key: dictionaryMap.keySet()) {
@@ -46,16 +55,32 @@ public class Dictionary {
 		return cand;
 	}
 	
+	/**
+	 * 日本語の単語を英語に変換する
+	 * @param word
+	 * @return
+	 */
 	public String translate(String word) {
 		return dictionaryMap.get(word);
 	}
 	
+	/**
+	 * 日本語を snake case の英語に変換する
+	 * @param japanese
+	 * @return
+	 */
 	public List<String> transList(String japanese) {
 		List<String> trans = new ArrayList<>();
-		while (!japanese.equals("")) {
-			String word = extractBeginWord(japanese);
+		String prev, next;
+		prev = next = japanese;
+		while (!next.equals("")) {
+			String word = extractBeginWord(next);
 			trans.add(translate(word));
-			japanese = japanese.substring(word.length());
+			prev = next;
+			next = prev.substring(word.length());
+			if (next.length() > prev.length()) {
+				break;
+			}
 		}
 		return trans;
 	}
--- src/ob/Translator.java
+++ src/ob/Translator.java
@@ -20,15 +20,21 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class Transrator {
+public class Translator {
 
 	private static final String ENTITY = "ENTITY";
 	private static final String L_NAME = "L-NAME";
 	private static final String P_NAME = "P-NAME";
 	private static final String ATTR = "ATTR";
+	private static final String ENCODING = "UTF-8";
+	private static final String DEFAULT_OUTPUT = "output.edm";
 	
 	private static Dictionary dictionary;
 	
+	/**
+	 * 日本語のモデルを英語のモデルに変換する
+	 * @param args: {辞書ファイル, 日本語のモデル, [出力先]}
+	 */
 	public static void main(String[] args) {
 		if (args.length != 2 && args.length != 3) {
 			System.err.println("usage: dictionary_file.csv japanese_model.edm [english_model.edm]");
@@ -58,9 +64,9 @@ public class Transrator {
 				}
 			}
 			
-			String outputFileName = (args.length == 3) ? args[2] : "output.edm";
+			String outputFileName = (args.length == 3) ? args[2] : DEFAULT_OUTPUT;
 			File output = new File(outputFileName);
-			if (write(output, (Document) root, "UTF-8")) {
+			if (write(output, (Document) root, ENCODING)) {
 				System.out.println("success");
 			} else {
 				System.out.println("failed");
@@ -75,6 +81,12 @@ public class Transrator {
 		}
 	}
 	
+	/**
+	 * ノード中のP_NAMEを英語に変換する
+	 * @param n
+	 * @return true : 変換できた
+	 *          false: 変換失敗 
+	 */
 	private static boolean nodeTranslator(Node n) {
 		NamedNodeMap attr = n.getAttributes();
 		Node lName = attr.getNamedItem(L_NAME);
